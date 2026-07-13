968dba0e743e5cde4c681ee3d50b3d070b0927b5
TomerP750 <tomer.jcb65@gmail.com>
2026-07-13 10:37:31 +0300

    ui fix

--- frontend/src/features/dashboard/index/components/LearningPathCard.tsx
+++ frontend/src/features/dashboard/index/components/LearningPathCard.tsx
@@ -16,21 +16,21 @@ export function LearningPathCard({ learningPath }: LearningPathCardProps) {
     const { id, title, description, createdAt, topicsLength, completedTopicsCount } = learningPath;
 
     return (
-        <article className="group rounded-xl border border-neutral-200 bg-white p-5 
-        shadow-sm transition hover:shadow-md hover:translate-y-[-2px]">
+        <article className="group rounded-xl border border-neutral-200 bg-white dark:border-zinc-700 dark:bg-zinc-900 p-5 
+        shadow-sm dark:shadow-zinc-800 transition hover:shadow-md hover:translate-y-[-2px]">
             <div className="flex flex-col gap-4">
                 {/* Header */}
                 <div className="flex items-start justify-between">
                     <div className="flex items-center gap-3">
-                        <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-primary/10 text-primary">
+                        <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-primary/10 text-primary dark:bg-primary/10 dark:text-zinc-100">
                             <Map size={30} strokeWidth={1.2} />
                         </div>
 
                         <div>
-                            <h3 className="text-lg font-semibold text-neutral-900">
+                            <h3 className="text-lg font-semibold text-neutral-900 dark:text-zinc-100">
                                 {title}
                             </h3>
-                            <p className="text-sm text-neutral-500">
+                            <p className="text-sm text-neutral-500 dark:text-zinc-400">
                                 Created {createdAt.toLocaleDateString()}
                             </p>
                         </div>
@@ -42,7 +42,7 @@ export function LearningPathCard({ learningPath }: LearningPathCardProps) {
                             leftIcon={<MoreVertical size={18} />}
                             variant="ghost"
                             size="sm"
-                            className="p-2! text-neutral-500 hover:text-neutral-700"
+                            className="p-2! text-neutral-500 hover:text-neutral-700 dark:text-zinc-400 dark:hover:text-zinc-200"
                         />
 
                         <CrudMenu
@@ -57,7 +57,7 @@ export function LearningPathCard({ learningPath }: LearningPathCardProps) {
                 </div>
 
                 {/* Description */}
-                <p className="line-clamp-2 text-sm text-neutral-600">
+                <p className="line-clamp-2 text-sm text-neutral-600 dark:text-zinc-400">
                     {description}
                 </p>
 
@@ -67,13 +67,13 @@ export function LearningPathCard({ learningPath }: LearningPathCardProps) {
 
 
                 {/* Footer */}
-                <div className="flex items-center justify-between border-t border-neutral-100 pt-4">
-                    <div className="flex gap-4 text-sm text-neutral-500">
+                <div className="flex items-center justify-between border-t border-neutral-100 dark:border-zinc-700 pt-4">
+                    <div className="flex gap-4 text-sm text-neutral-500 dark:text-zinc-400">
                         <span>{topicsLength} Topics</span>
                         <span>4 Sections</span>
                     </div>
 
-                    <button className="text-sm font-medium text-primary hover:underline">
+                    <button className="text-sm font-medium text-primary hover:underline dark:text-zinc-400 dark:hover:text-zinc-200">
                         Continue →
                     </button>
                 </div>
