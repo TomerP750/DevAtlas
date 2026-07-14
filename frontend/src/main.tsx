cdcdec6223e55d92373a59bf97546642efa0d515
TomerP750 <tomer.jcb65@gmail.com>
2026-07-14 14:53:04 +0300

    authprovider added to main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,16 +5,19 @@ import { Layout } from './layout/Layout'
 import { ThemeProvider } from './shared/contexts/ThemeContext'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ToastConfig } from './shared/utils/ToastConfig'
+import { AuthProvider } from './features/authentication/contexts/AuthContext'
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-      <ThemeProvider>
-        <ToastConfig />
-        <Layout />
-      </ThemeProvider>
+      <AuthProvider>
+        <ThemeProvider>
+          <ToastConfig />
+          <Layout />
+        </ThemeProvider>
+      </AuthProvider>
     </BrowserRouter>
   </QueryClientProvider>,
 )
