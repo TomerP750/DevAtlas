2a9e64da48a45104def4b710ea4b648f007c3799
TomerP750 <tomer.jcb65@gmail.com>
2026-08-06 16:56:55 +0300

    auth init

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { UserService } from './user/user.service';
 import { User } from './user/user.entity';
 import { UserModule } from './user/user.module';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
   // imports: [TypeOrmModule.forRoot({
@@ -12,7 +13,7 @@ import { UserModule } from './user/user.module';
   //   synchronize: true,
 
   // }), UserService],
-  imports: [UserModule],
+  imports: [UserModule, AuthenticationModule],
   providers: [AppService],
   controllers: [AppController],
 })
