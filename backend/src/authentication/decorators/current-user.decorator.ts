1b09a6d1fb5e83a368df618c7f57c948472b35af
TomerP750 <tomer.jcb65@gmail.com>
2026-08-09 14:39:59 +0300

    fix

--- backend/src/authentication/decorators/current-user.decorator.ts
+++ backend/src/authentication/decorators/current-user.decorator.ts
@@ -2,9 +2,9 @@ import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 
 
 /**
- * Decorator to get the current user's ID from the request.
+ * Decorator to get the current user from the request.
  */
-export const CurrentUser = createParamDecorator(
+export const CurrentUserId = createParamDecorator(
     (data: never, ctx: ExecutionContext): string | undefined => {
         const request = ctx.switchToHttp().getRequest();
         return request.user?.sub;
--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,11 +1,10 @@
 import { Body, Controller, Delete, Get, Param, Put, UseGuards } from '@nestjs/common';
-import { Serialize } from 'src/shared/interceptors/serialize.interceptor';
 import { UpdateUserDto } from './dtos/update-user-dto';
 import { UserDto } from './dtos/user.dto';
 import { UserService } from './user.service';
-import { CurrentUser } from './decorators/current-user.decorator';
-import { User } from './user.entity';
-// import { AuthGuard } from 'src/guards/auth.guard';
+import { CurrentUserId } from '../authentication/decorators/current-user.decorator';
+import { Serialize } from '../shared/interceptors/serialize.interceptor';
+
 
 @Controller('user')
 @Serialize(UserDto)
@@ -16,21 +15,18 @@ export class UserController {
     ) {}
 
     @Put("/update")
-    // @UseGuards(AuthGuard)
-    updateUser(@CurrentUser() user: User, @Body() dto: UpdateUserDto) {
-        this.userService.update(user.id, dto)
+    updateUser(@CurrentUserId() userId: string, @Body() dto: UpdateUserDto) {
+        this.userService.update(userId, dto)
     }
 
-    @Get("/:id")
-    // @UseGuards(AuthGuard)
-    findUser(@Param("id") id: string) {
-        return this.userService.findOne(id);
+    @Get("/me")
+    findUser(@CurrentUserId() userId: string) {
+        return this.userService.findOne(userId);
     }
 
     @Delete("/:id")
-    // @UseGuards(AuthGuard)
-    deleteUser(@CurrentUser() user: User) {
-        this.userService.remove(user.id);
+    deleteUser(@CurrentUserId() userId: string) {
+        this.userService.remove(userId);
     }
 
 }
