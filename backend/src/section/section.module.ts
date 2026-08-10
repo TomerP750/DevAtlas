72a9ecb88571c2e33011ba9e39a24b2f1bae48f9
TomerP750 <tomer.jcb65@gmail.com>
2026-08-10 11:36:59 +0300

    module

--- backend/src/section/section.module.ts
+++ backend/src/section/section.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SectionService } from './section.service';
 import { SectionController } from './section.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TopicModule } from '../topic/topic.module';
+import { Section } from './section.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Section]), TopicModule],
   providers: [SectionService],
   controllers: [SectionController]
 })
